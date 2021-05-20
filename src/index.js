0e813250b8ddc2979d6880e9a3a0b1ff31125a91
jimmyvargaslozanojr <zirymmij@gmail.com>
2021-05-21 01:11:26 +0800

    prop key

--- src/index.js
+++ src/index.js
@@ -4,48 +4,60 @@ import ReactDom from "react-dom";
 // CSS
 import './index.css';
 // Setup Variables
-const firstbook = {
-  img:'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
-  title : 'I Love You to the Moon and Back',
-  author : 'Amelia Hepworth',
-}
+const books = [
+  {
+    id:1,
+    img:'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+    title : 'I Love You to the Moon and Back',
+    author : 'Amelia Hepworth',
+  },
+  {
+    id:2,
+    img:'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg',
+    title : 'Our Class is a Family',
+    author : 'Shannon Olsen',
+  },
+  {
+    id:3,
+    img:'https://images-na.ssl-images-amazon.com/images/I/81QRyjf28tS._AC_UL200_SR200,200_.jpg',
+    title : "Oh, the Places You'll Go!",
+    author : 'Dr. Seuss',
+  }
+]
 
-const secondbook = {
-  img:'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg',
-  title : 'Our Class is a Family',
-  author : 'Shannon Olsen',
-}
 
-// const title = 'I Love You to the Moon and Back';
-// const author = 'Amelia Hepworth';
-// const img = 'https://images-na.ssl-images-amazon.com/images/I/81NdWmk6aUL._AC_UL200_SR200,200_.jpg';
 
 function BookList() {
   return (  
       <section className="booklist">
-        <Book img={firstbook.img} title={firstbook.title} author={firstbook.author}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veritatis, quo quas omnis voluptates, quidem facilis voluptas iusto dolore consectetur, qui vitae in. Fugit similique veniam ratione porro, illum aperiam velit?</Book>
-
-        <Book img={secondbook.img} title={secondbook.title} author={secondbook.author}/>
+        {books.map((book) => {
+          // const {img, title, author} = book;
+          return <Book key={book.id} book={book}></Book>;
+        })}
       </section>
   );
 }
 
-const Book = ({img,title,author,children}) => {
-  // const {img,title, author} = props;
+const Book = (props) => {
+  const {img,title, author} = props.book;
   return <article className="book">
     <img src={img} alt=""/>
     <h1>{title}</h1>
     <h4>{author}</h4>
-    {children}
+    {props.children}
   </article>
 }
 
 
-ReactDom.render(<BookList />, document.getElementById("root"));
+ReactDom.render(<BookList />, 
+  document.getElementById("root"));
 
 
 
-
+  // const names = ['Jimmy','Rylie','Kyle Daniel'];
+  // const newNames = names.map((name)=> {
+  //   return <h1>{name}</h1>;
+  // });
 
 // const Image = () => {
 //   return <img src="https://images-na.ssl-images-amazon.com/images/I/81NdWmk6aUL._AC_UL200_SR200,200_.jpg" alt="" />
