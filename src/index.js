b9a10bb2405617771c66236a01c0adf1749c81f3
jimmyvargaslozanojr <zirymmij@gmail.com>
2021-05-20 23:49:05 +0800

    add props and object var

--- src/index.js
+++ src/index.js
@@ -3,40 +3,53 @@ import ReactDom from "react-dom";
 
 // CSS
 import './index.css';
+// Setup Variables
+const firstbook = {
+  img:'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
+  title : 'I Love You to the Moon and Back',
+  author : 'Amelia Hepworth',
+}
+
+const secondbook = {
+  img:'https://m.media-amazon.com/images/I/71aLultW5EL._AC_UY218_.jpg',
+  title : 'Our Class is a Family',
+  author : 'Shannon Olsen',
+}
+
+
+// const title = 'I Love You to the Moon and Back';
+// const author = 'Amelia Hepworth';
+// const img = 'https://images-na.ssl-images-amazon.com/images/I/81NdWmk6aUL._AC_UL200_SR200,200_.jpg';
 
 function BookList() {
   return (  
       <section className="booklist">
-        <Book/>
-        <Book/>
-        <Book/>
-        <Book/>
-        <Book/>
-        <Book/>
-        <Book/>
+        <Book img={firstbook.img} title={firstbook.title} author={firstbook.author}/>
+        <Book img={secondbook.img} title={secondbook.title} author={secondbook.author}/>
       </section>
   );
 }
 
-const Book = () => {
+const Book = (props) => {
   return <article className="book">
-    <Image />
-    <Title />
-    <Author />
+    <img src={props.img} alt=""/>
+    <h1>{props.title}</h1>
+    <h4>{props.author}</h4>
   </article>
 }
 
-const Image = () => {
-  return <img src="https://images-na.ssl-images-amazon.com/images/I/81NdWmk6aUL._AC_UL200_SR200,200_.jpg" alt="" />
-}
-
-const Title =()=> <h1>Zero Fail</h1>;
-const Author = () => <p>Jimmy Lozano</p>;
 
 ReactDom.render(<BookList />, document.getElementById("root"));
 
 
 
+
+
+// const Image = () => {
+//   return <img src="https://images-na.ssl-images-amazon.com/images/I/81NdWmk6aUL._AC_UL200_SR200,200_.jpg" alt="" />
+// }
+// const Title =()=> <h1>Zero Fail</h1>;
+// const Author = () => <h4 style={{color:'#617d98',fontSize:'0.75rem',marginTop:'0.25rem'}}>Jimmy Lozano</h4>;
 
 
 // const Greeting =() =>{
