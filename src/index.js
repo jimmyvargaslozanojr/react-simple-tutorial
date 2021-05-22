f1f5236013437ffad440a65d674a553a490eab6b
jimmyvargaslozanojr <zirymmij@gmail.com>
2021-05-22 11:35:59 +0800

    Events

--- src/index.js
+++ src/index.js
@@ -38,13 +38,29 @@ function BookList() {
   );
 }
 
+
 const Book = (props) => {
+
+  const clickHandler = (e) => {
+    console.log(e);
+    console.log(e.target);
+  };
+
+  const complexExample = (author) => {
+    console.log(author);
+  };
+  
+
   const {img,title, author} = props;
-  return <article className="book">
+  return <article className="book" onMouseOver={()=>{
+    console.log(title);
+  }}>
     <img src={img} alt=""/>
-    <h1>{title}</h1>
+    <h1 onClick={() => console.log(title)}>{title}</h1>
     <h4>{author}</h4>
     {props.children}
+    <button type="button" onClick={clickHandler}>ClickMe</button>
+    <button type="button" onClick={() => complexExample(author)}>More Complex Example</button>
   </article>
 }
 
